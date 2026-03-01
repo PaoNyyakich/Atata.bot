37b2802b5abc1c86b0cf6fef0f8ddaec19f896c0
PaoNyyakich <paonyao5@gmail.com>
2026-03-01 20:45:53 +0300

    Upload JS

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 let tg = window.Telegram.WebApp;
 
-let btn = document.querySelector('#click');
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
 
-btn.onclick = () => {
-    if (tg.MainButton.InVisible) {
-        tg.MainButton.hide()
-    } else {
-        tg.MainButton.setText('Вы выбрали котика!')
-        tg.MainButton.show()
-    }
-}
+let item = "";
 
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
 
-tg.expand()
+btn1.onclick = () => {
+    tg.MainButton.setText("Вы выбрали Москва-Нижний Новгород!");
+    item = "Москва-Нижний Новгород";
+    tg.MainButton.show();
+};
+
+btn2.onclick = () => {
+    tg.MainButton.setText("Вы выбрали Самара-Казань!");
+    item = "Самара-Казань";
+    tg.MainButton.show();
+};
+
+btn3.onclick = () => {
+    tg.MainButton.setText("Вы выбрали Ижевск-Брянск!");
+    item = "Ижевск-Брянск";
+    tg.MainButton.show();
+};
+
+btn4.onclick = () => {
+    tg.MainButton.setText("Вы выбрали Владивосток-Сочи!");
+    item = "Владивосток-Сочи";
+    tg.MainButton.show();
+};
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
+
+tg.expand();
